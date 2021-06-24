a6faa16bbefdd5009f40e349b80c2b593c5f8ae6
nicklu717 <nicklu717@gmail.com>
2021-06-24 17:53:01 +0800

    [feat] add LinkedList add-value APIs & tests

--- Sources/DataStructure/LinkedList.swift
+++ Sources/DataStructure/LinkedList.swift
@@ -7,16 +7,73 @@
 
 public struct LinkedList<Value> {
     
-    public private(set) var head: Node<Value>?
-    public private(set) var tail: Node<Value>?
-    
-    public init(head: Node<Value>? = nil) {
-        self.head = head
-        self.tail = head
+    enum Error: Swift.Error {
+        case invalidIndex
+        case indexOutOfRange
     }
     
-    public init(headValue: Value) {
-        self.init(head: Node<Value>(value: headValue))
+    private var head: Node<Value>?
+    private var tail: Node<Value>?
+    
+    public private(set) var count: Int = 0
+    public var isEmpty: Bool {
+        return count == 0
+    }
+    
+    public init() {}
+    
+    public init(values: [Value]) {
+        values.forEach {
+            append($0)
+        }
+    }
+    
+    subscript(index: Int) -> Value {
+        let node = getNode(for: index)
+        guard let value = node?.value else { fatalError("Index out of range") }
+        return value
+    }
+    
+    public mutating func push(_ value: Value) {
+        head = Node<Value>(value: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+        count += 1
+    }
+    
+    public mutating func append(_ value: Value) {
+        guard !isEmpty else {
+            push(value)
+            return
+        }
+        tail?.next = Node<Value>(value: value)
+        tail = tail?.next
+        count += 1
+    }
+    
+    public mutating func insert(_ value: Value, at index: Int) throws {
+        if index < 0 {
+            throw Error.invalidIndex
+        } else if index > count {
+            throw Error.indexOutOfRange
+        } else if index == 0 {
+            push(value)
+        } else if index == count {
+            append(value)
+        } else {
+            let previousNode = getNode(for: index - 1)
+            previousNode?.next = Node<Value>(value: value, next: previousNode?.next)
+            count += 1
+        }
+    }
+    
+    private func getNode(for index: Int) -> Node<Value>? {
+        var node = head
+        for _ in 0..<index {
+            node = node?.next
+        }
+        return node
     }
 }
 
--- Tests/DataStructureTests/LinkedListTests.swift
+++ Tests/DataStructureTests/LinkedListTests.swift
@@ -14,30 +14,123 @@ final class LinkedListTestCase: XCTestCase {
     private var linkedList: LinkedList<Int>!
     
     // MARK: - Life Cycle
-    
     override func tearDown() {
         linkedList = nil
         super.tearDown()
     }
     
-    // MARK: - Tests
-    func testInitWithNoHead() {
+    // MARK: - Tests: Initialize
+    func testInit() {
         linkedList = LinkedList()
-        XCTAssert(linkedList.head == nil)
-        XCTAssert(linkedList.tail == nil)
-    }
-    
-    func testInitWithHead() {
-        let headNode = Node<Int>(value: 10)
         
-        linkedList = LinkedList(head: headNode)
-        XCTAssert(linkedList.head === headNode)
-        XCTAssert(linkedList.tail === headNode)
+        XCTAssert(linkedList.count == 0)
+        XCTAssert(linkedList.isEmpty)
     }
     
-    func testInitWithValue() {
-        linkedList = LinkedList(headValue: 20)
-        XCTAssert(linkedList.head?.value == 20)
-        XCTAssert(linkedList.tail?.value == 20)
+    func testInitWithArray() {
+        linkedList = LinkedList(values: [0, 1, 2])
+        
+        XCTAssert(linkedList.count == 3)
+        XCTAssert(!linkedList.isEmpty)
+    }
+    
+    // MARK: - Tests: Push
+    func testPushOneValue() {
+        linkedList = LinkedList()
+        linkedList.push(0)
+        
+        XCTAssert(linkedList[0] == 0)
+        XCTAssert(linkedList.count == 1)
+    }
+    
+    func testPushMultipleValues() {
+        linkedList = LinkedList()
+        linkedList.push(2)
+        linkedList.push(1)
+        linkedList.push(0)
+        
+        XCTAssert(linkedList[0] == 0)
+        XCTAssert(linkedList[1] == 1)
+        XCTAssert(linkedList[2] == 2)
+        XCTAssert(linkedList.count == 3)
+    }
+    
+    // MARK: - Tests: Append
+    func testAppendOneValue() {
+        linkedList = LinkedList()
+        linkedList.append(0)
+        
+        XCTAssert(linkedList[0] == 0)
+        XCTAssert(linkedList.count == 1)
+    }
+    
+    func testAppendMultipleValues() {
+        linkedList = LinkedList()
+        linkedList.append(0)
+        linkedList.append(1)
+        linkedList.append(2)
+        
+        XCTAssert(linkedList[0] == 0)
+        XCTAssert(linkedList[1] == 1)
+        XCTAssert(linkedList[2] == 2)
+        XCTAssert(linkedList.count == 3)
+    }
+    
+    // MARK: - Tests: Insert
+    func testInsert() {
+        linkedList = LinkedList(values: [0, 1, 2])
+        XCTAssertNoThrow(try linkedList.insert(10, at: 2))
+        
+        XCTAssert(linkedList[0] == 0)
+        XCTAssert(linkedList[1] == 1)
+        XCTAssert(linkedList[2] == 10)
+        XCTAssert(linkedList[3] == 2)
+        XCTAssert(linkedList.count == 4)
+    }
+    
+    func testInsertAtHead() {
+        linkedList = LinkedList(values: [0, 1, 2])
+        XCTAssertNoThrow(try linkedList.insert(-1, at: 0))
+        
+        XCTAssert(linkedList[0] == -1)
+        XCTAssert(linkedList[1] == 0)
+        XCTAssert(linkedList[2] == 1)
+        XCTAssert(linkedList[3] == 2)
+        XCTAssert(linkedList.count == 4)
+    }
+    
+    func testInsertAtTail() {
+        linkedList = LinkedList(values: [0, 1, 2])
+        XCTAssertNoThrow(try linkedList.insert(3, at: 3))
+        
+        XCTAssert(linkedList[0] == 0)
+        XCTAssert(linkedList[1] == 1)
+        XCTAssert(linkedList[2] == 2)
+        XCTAssert(linkedList[3] == 3)
+        XCTAssert(linkedList.count == 4)
+    }
+    
+    func testInsertWhenEmpty() {
+        linkedList = LinkedList()
+        XCTAssertNoThrow(try linkedList.insert(0, at: 0))
+        
+        XCTAssert(linkedList[0] == 0)
+        XCTAssert(linkedList.count == 1)
+    }
+    
+    func testInsertAtOutOfRangeIndex() {
+        linkedList = LinkedList()
+        var error: Error?
+        
+        XCTAssertThrowsError(try linkedList.insert(0, at: 1)) { error = $0 }
+        XCTAssert((error as! LinkedList<Int>.Error) == .indexOutOfRange)
+    }
+    
+    func testInsertAtInvalidIndex() {
+        linkedList = LinkedList()
+        var error: Error?
+        
+        XCTAssertThrowsError(try linkedList.insert(0, at: -1)) { error = $0 }
+        XCTAssert((error as! LinkedList<Int>.Error) == .invalidIndex)
     }
 }
