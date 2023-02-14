dd63b2a52b0c246a754e52e8abd020da8b7e95c8
Nick Lu <nicklu717@gmail.com>
2023-02-14 13:16:32 +0800

    [test] BinarySearchTree

---
+++ Tests/DataStructureTests/BinaryTree/BinarySearchTreeTests.swift
@@ -0,0 +1,73 @@
+//
+//  BinarySearchTreeTests.swift
+//  DataStructureTests
+//
+//  Created by 陸瑋恩 on 2023/2/14.
+//
+
+import XCTest
+@testable import DataStructure
+
+class BinarySearchTreeTests: XCTestCase {
+    
+    func testInitWithNoRoot() {
+        let tree = BinarySearchTree<Int>(element: nil, compare: { _, _ in .equal })
+        XCTAssertNil(tree.root)
+    }
+    
+    func testInitWithIntRoot() {
+        let tree = BinarySearchTree<Int>(element: 88, compare: { _, _ in .equal })
+        XCTAssertEqual(tree.root?.element, 88)
+    }
+    
+    func testInitWithStringRoot() {
+        let tree = BinarySearchTree<String>(element: "88", compare: { _, _ in .equal })
+        XCTAssertEqual(tree.root?.element, "88")
+    }
+    
+    func testInsert() {
+        let tree = BinarySearchTree<Int>(element: nil, compare: {
+            switch $0 {
+            case ..<$1:
+                return .lessThan
+            case $1:
+                return .equal
+            default:
+                return .largerThan
+            }
+        })
+        
+        tree.insert(element: 9)
+        XCTAssertEqual(tree.root?.element, 9)
+        
+        tree.insert(element: 3)
+        tree.insert(element: 20)
+        XCTAssertEqual(tree.root?.left?.element, 3)
+        XCTAssertEqual(tree.root?.right?.element, 20)
+        
+        tree.insert(element: 8)
+        XCTAssertEqual(tree.root?.left?.right?.element, 8)
+        tree.insert(element: 92)
+        XCTAssertEqual(tree.root?.right?.right?.element, 92)
+    }
+    
+    func testInsertDuplicatedElement() {
+        let tree = BinarySearchTree<Int>(element: nil, compare: {
+            switch $0 {
+            case ..<$1:
+                return .lessThan
+            case $1:
+                return .equal
+            default:
+                return .largerThan
+            }
+        })
+        
+        tree.insert(element: 9)
+        tree.insert(element: 9)
+        tree.insert(element: 9)
+        XCTAssertEqual(tree.root?.element, 9)
+        XCTAssertNil(tree.root?.left)
+        XCTAssertNil(tree.root?.right)
+    }
+}
